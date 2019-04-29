96dbafb6560d8d5c317d4c84abb36e7ad0cba23f
tharun <tharuneniyan4@gmail.com>
2019-04-29 14:15:35 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, Image, View} from 'react-native';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.half}>
+
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+
+});
